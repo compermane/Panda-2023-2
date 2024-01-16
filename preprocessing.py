89c8fc7c73c4353144ea7422a0266cce3b2030f8
compermane <comper.mane@gmail.com>
2024-01-15 23:25:34 -0300

    Correções

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import csv
 from typing import List
 
 class Estudante:
-    def __init__(self, notaMT, notaLP, notaCN, notaCH, notaRED, sexo, corRaca, tpEscola, faixaEtaria) -> None:
+    def __init__(self, notaMT, notaLP, notaCN, notaCH, notaRED, sexo, corRaca, tpEscola, faixaEtaria, q006) -> None:
         self._notaMT = notaMT if notaMT != "" else 0
         self._notaLP = notaLP if notaLP != "" else 0
         self._notaCN = notaCN if notaCN != "" else 0
@@ -15,11 +15,13 @@ class Estudante:
         elif corRaca == "1":
             self._corRaca = "Branca"
         elif corRaca == "2":
-            self._corRaca = "Parda"
+            self._corRaca = "Preta"
         elif corRaca == "3":
-            self._corRaca = "Amarela"
+            self._corRaca = "Parda"
         elif corRaca == "4":
-            self._corRaca = "Indigena"
+            self._corRaca = "Amarela"
+        elif corRaca == "5":
+            self._corRaca = "Indígena"
         else:
             self._corRaca = "Nao dispoe da informacao"
 
@@ -71,6 +73,40 @@ class Estudante:
         else:
             self._faixaEtaria = "Maior de 70 anos"
 
+        if q006 == "A":
+            self._q006 = "Nenhuma Renda"
+        elif q006 == "B":
+            self._q006 = "Até R$ 1.212,00"
+        elif q006 == "C":
+            self._q006 = "De R$ 1.212,01 até R$ 1.818,00."
+        elif q006 == "D":
+            self._q006 = "De R$ 1.818,01 até R$ 2.424,00."
+        elif q006 == "E":
+            self._q006 = "De R$ 2.424,01 até R$ 3.030,00."
+        elif q006 == "F":
+            self._q006 = "De R$ 3.030,01 até R$ 3.636,00."
+        elif q006 == "G":
+            self._q006 = "De R$ 3.636,01 até R$ 4.848,00."
+        elif q006 == "H":
+            self._q006 = "De R$ 4.848,01 até R$ 6.060,00."
+        elif q006 == "I":
+            self._q006 = "De R$ 6.060,01 até R$ 7.272,00."
+        elif q006 == "J":
+            self._q006 = "De R$ 7.272,01 até R$ 8.484,00."
+        elif q006 == "K":
+            self._q006 = "De R$ 8.484,01 até R$ 9.696,00."
+        elif q006 == "L":
+            self._q006 = "De R$ 9.696,01 até R$ 10.908,00."
+        elif q006 == "M":
+            self._q006 = "De R$ 10.908,01 até R$ 12.120,00."
+        elif q006 == "N":
+            self._q006 = "De R$ 12.120,01 até R$ 14.544,00."
+        elif q006 == "O":
+            self._q006 = "De R$ 14.544,01 até R$ 18.180,00."
+        elif q006 == "P":
+            self._q006 = "De R$ 18.180,01 até R$ 24.240,00."
+        elif q006 == "Q":
+            self._q006 = "Acima de R$ 24.240,00."
     @property
     def notaMT(self):
         return self._notaMT
@@ -107,6 +143,10 @@ class Estudante:
     def faixaEtaria(self):
         return self._faixaEtaria
     
+    @property
+    def q006(self):
+        return self._q006
+    
     def calculaMedia(self) -> float:
         sum = float(self.notaCH) + float(self.notaCN) + float(self.notaLP) + float(self.notaMT) + float(self.notaRED)
         return round((sum / 5), 2)
@@ -117,7 +157,7 @@ def readData() -> List[Estudante]:
         estudantes: List[Estudante] = []
 
         for row in csv_reader:
-            estudantes.append(Estudante(row["NU_NOTA_MT"], row["NU_NOTA_LC"], row["NU_NOTA_CN"], row["NU_NOTA_CH"], row["NU_NOTA_REDACAO"], row["TP_SEXO"], row["TP_COR_RACA"], row["TP_ESCOLA"], row["TP_FAIXA_ETARIA"]))
+            estudantes.append(Estudante(row["NU_NOTA_MT"], row["NU_NOTA_LC"], row["NU_NOTA_CN"], row["NU_NOTA_CH"], row["NU_NOTA_REDACAO"], row["TP_SEXO"], row["TP_COR_RACA"], row["TP_ESCOLA"], row["TP_FAIXA_ETARIA"], row["Q006"]))
         f.close()
 
     return estudantes
@@ -125,12 +165,12 @@ def readData() -> List[Estudante]:
 def writeData(estudantes: List[Estudante]) -> None:
     with open("Microdados_do_enem.csv", "w", newline = '') as f:
         writer = csv.writer(f)
-        header = ["NU_NOTA_MT", "NU_NOTA_LC", "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_REDACAO", "MEDIA_SIMPLES", "TP_ESCOLA", "TP_SEXO", "TP_COR_RACA", "TP_FAIXA_ETARIA"]
+        header = ["NU_NOTA_MT", "NU_NOTA_LC", "NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_REDACAO", "MEDIA_SIMPLES", "TP_ESCOLA", "TP_SEXO", "TP_COR_RACA", "TP_FAIXA_ETARIA", "Q006"]
 
         writer.writerow(header)
 
         for est in estudantes:
-            row = [est.notaMT, est.notaLP, est.notaCN, est.notaCH, est.notaRED, est.calculaMedia(), est.tpEscola, est.sexo, est.corRaca, est.faixaEtaria]
+            row = [est.notaMT, est.notaLP, est.notaCN, est.notaCH, est.notaRED, est.calculaMedia(), est.tpEscola, est.sexo, est.corRaca, est.faixaEtaria, est.q006]
             writer.writerow(row)
 
     f.close()
